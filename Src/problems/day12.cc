fed9f32a5f77cd49e358e496fce0929361b92bcc
Johan Levin <johan13@gmail.com>
2023-12-17 16:27:38 +0100

    Optimize day 12 (to <30 seconds at O3)

--- Src/problems/day12.cc
+++ Src/problems/day12.cc
@@ -39,35 +39,65 @@ static int64_t count_arrangements(std::string_view wells, std::span<const int> g
     if (wells.empty())
         return 0;
 
-    // Try to bail out early
-    int min_wells = 0, max_wells = 0;
-    for (char c : wells) {
+    // Pick the middle element in 'groups' and find at what index ('i') in 'wells' it can start.
+    int group_ix = groups.size() / 2;
+    int group = groups[group_ix];
+    auto left_groups = groups.subspan(0, group_ix);
+    auto right_groups = groups.subspan(group_ix + 1);
+
+    // Find a start_i and end_i that limits the search range so that there are enough '#' and '?'
+    // on either side to satisfy the required groups.
+    int left_separators = left_groups.size() - 1;
+    int left_working = std::ranges::fold_left(left_groups, 0, std::plus());
+    int start_i = 0;
+    while (left_separators > 0 || left_working > 0) {
+        char c = wells[start_i++];
         if (c == '#') {
-            ++min_wells;
-            ++max_wells;
+            --left_working;
         } else if (c == '?') {
-            ++max_wells;
+            --left_working;
+            --left_separators;
         }
     }
-    int num_wells = std::ranges::fold_left(groups, 0, std::plus());
-    if (num_wells < min_wells || num_wells > max_wells) return 0;
+
+    int right_separators = right_groups.size() - 1;
+    int right_working = std::ranges::fold_left(right_groups, 0, std::plus());
+    int end_i = wells.length();
+    while (right_separators > 0 || right_working > 0) {
+        char c = wells[--end_i];
+        if (c == '#') {
+            --right_working;
+        } else if (c == '?') {
+            --right_working;
+            --right_separators;
+        }
+    }
+
+    end_i -= group;
+    int mid_i = (start_i + end_i) / 2;
 
     int64_t count = 0;
-    int mid_group_ix = groups.size() / 2;
-    int mid_group_sz = groups[mid_group_ix];
-    for (int i = 0; i <= (int)wells.length() - mid_group_sz; ++i) {
+    for (int i = start_i; i <= end_i; ++i) {
         if (i - 1 >= 0 && wells[i - 1] == '#') continue; // No start separator
-        if (i + mid_group_sz < (int)wells.length() && wells[i + mid_group_sz] == '#') continue; // No end sep
-        if (wells.substr(i, mid_group_sz).find('.') != std::string_view::npos) continue; // Includes working well
+        if (i + group < (int)wells.length() && wells[i + group] == '#') continue; // No end sep
+        if (wells.substr(i, group).find('.') != std::string_view::npos) continue; // Includes working well
 
         auto left_wells = wells.substr(0, std::max(0, i - 1));
-        auto right_wells = wells.substr(std::min(i + mid_group_sz + 1, (int)wells.length()));
-        auto left_groups = groups.subspan(0, mid_group_ix);
-        auto right_groups = groups.subspan(mid_group_ix + 1);
+        auto right_wells = wells.substr(std::min(i + group + 1, (int)wells.length()));
 
-        int64_t lhs = count_arrangements(left_wells, left_groups);
-        int64_t rhs = count_arrangements(right_wells, right_groups);
-        count += lhs * rhs;
+        if (i < mid_i) {
+            int64_t lhs = count_arrangements(left_wells, left_groups);
+            if (lhs > 0) {
+                int64_t rhs = count_arrangements(right_wells, right_groups);
+                count += lhs * rhs;
+            }
+        } else {
+            int64_t rhs = count_arrangements(right_wells, right_groups);
+            if (rhs > 0) {
+                int64_t lhs = count_arrangements(left_wells, left_groups);
+                count += lhs * rhs;
+            }
+        }
     }
 
     return count;
